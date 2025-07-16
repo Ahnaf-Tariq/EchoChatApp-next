e4129cfb3763aa47aa94256bd09c97401e3bfd55
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-07-16 13:23:26 -0700

    login functionality done

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,94 @@
-import React from 'react'
+"use client";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [currentState, setCurrentState] = useState("Sign In");
   return (
-    <div>
-        
+    <div className="flex justify-center mt-20 items-center px-2">
+      <div className="bg-white shadow-md p-6 rounded-xl w-96">
+        <div className="flex justify-between">
+          <h1 className="font-semibold text-xl">{currentState}</h1>
+          <button className="text-xl">X</button>
+        </div>
+        <hr className="text-gray-400 my-4" />
+        <div className="flex flex-col gap-3">
+          {currentState === "Sign Up" && (
+            <div className="flex flex-col gap-1">
+              <label htmlFor="name">Name</label>
+              <input
+                className="border border-gray-400 rounded-md px-1"
+                type="text"
+                placeholder="Full Name..."
+                id="name"
+              />
+            </div>
+          )}
+          <div className="flex flex-col gap-1">
+            <label htmlFor="email">Email</label>
+            <input
+              className="border border-gray-400 rounded-md px-1"
+              type="email"
+              placeholder="Email..."
+              id="email"
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <label htmlFor="password">Password</label>
+            <input
+              className="border border-gray-400 rounded-md px-1"
+              type="password"
+              placeholder="Password..."
+              id="password"
+            />
+          </div>
+          <button
+            // onClick={signIn}
+            className="w-full font-semibold bg-[#14b8a6] text-white rounded-lg py-1 mt-2 cursor-pointer"
+          >
+            {currentState}
+          </button>
+          {/* </div> */}
+          <div className="flex items-center my-1">
+            <hr className="flex-grow border-t border-gray-300" />
+            <span className="mx-2 text-gray-700">Or</span>
+            <hr className="flex-grow border-t border-gray-300" />
+          </div>
+          <button
+            // onClick={googleLogin}
+            className="w-full border border-[#14b8a6] text-xl rounded-lg cursor-pointer flex justify-center"
+          >
+            {/* Google logo */}
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOHI2JLXYQ0StQ1vzNLvULyckAUF1uIUnoxg&s"
+              className="size-7"
+              alt=""
+            />
+          </button>
+        </div>
+        {currentState === "Sign In" ? (
+          <p
+            className="mt-4 text-lg"
+            onClick={() => setCurrentState("Sign Up")}
+          >
+            or dont have an account{" "}
+            <span className="text-[#14b8a6] text-base font-semibold cursor-pointer">
+              Sign Up
+            </span>
+          </p>
+        ) : (
+          <p
+            className="mt-4 text-lg"
+            onClick={() => setCurrentState("Sign In")}
+          >
+            back to{" "}
+            <span className="text-[#14b8a6] text-base font-semibold cursor-pointer">
+              Sign In
+            </span>
+          </p>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar";
+import Login from "./login/page";
 
 export default function Home() {
   return (
     <div >
       <Navbar />
+      <Login />
     </div>
   );
 }
