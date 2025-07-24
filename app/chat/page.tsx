2748c201bcd6ad90582c8f5db56fb559005290d9
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-07-24 14:19:46 -0700

    getting all users from firebase

--- app/chat/page.tsx
+++ app/chat/page.tsx
@@ -2,15 +2,26 @@
 import Navbar from "@/components/Navbar";
 import { onAuthStateChanged } from "firebase/auth";
 import React, { useContext, useEffect, useState } from "react";
-import { auth } from "../firebase/config";
+import { auth, db } from "../firebase/config";
 import { useRouter } from "next/navigation";
 import { AppContext } from "@/context/Context";
 import { HiDotsVertical } from "react-icons/hi";
 import { FaSearch } from "react-icons/fa";
+import { BsExclamationCircle } from "react-icons/bs";
+import { IoSend } from "react-icons/io5";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+
+interface User {
+  id:string;
+  username:string;
+  email:string;
+  lastSeen: number;
+}
 
 const Chat = () => {
   const { LoadUserData } = useContext(AppContext);
   const router = useRouter();
+  const [usersList,setUsersList] = useState<User[]>([])
 
   useEffect(() => {
     onAuthStateChanged(auth, (curuser) => {
@@ -21,7 +32,7 @@ const Chat = () => {
     });
   }, []);
 
-  const chatList = [
+  const usersLists = [
     {
       name: "Richard Sanford",
       message: "Hello, How are you?",
@@ -63,12 +74,25 @@ const Chat = () => {
       avatar: "https://randomuser.me/api/portraits/men/8.jpg",
     },
   ];
+
+  useEffect(() => {
+    const fetch = async () => {
+      const userRef = collection(db, "users");
+      const snap = await getDocs(userRef);
+
+      const users: User[] = snap.docs.map((doc) => doc.data() as User);
+
+      console.log(users);
+      setUsersList(users)
+    };
+    fetch();
+  }, []);
   return (
     <div>
       <Navbar />
-      <div className="max-w-6xl mx-auto my-10 grid grid-cols-[1fr_2fr]">
+      <div className="max-w-6xl mx-auto my-10 grid grid-cols-[1fr_2fr] h-[600px]">
         {/* users */}
-        <div className="bg-[#14b8a6] text-white">
+        <div className="bg-[#14b8a6] text-white border-r-2 border-gray-100">
           <div className="flex justify-between items-center p-6 gap-2">
             <h1 className="text-2xl font-semibold">ChatApp</h1>
             <p className="cursor-pointer">
@@ -83,8 +107,8 @@ const Chat = () => {
               className="bg-transparent text-white placeholder-white outline-none w-full"
             />
           </div>
-          <div>
-            {chatList.map((chat, index) => (
+          <div className="max-h-[450px] overflow-y-scroll scrollbar-hide">
+            {usersList.map((users, index) => (
               <div
                 key={index}
                 className={`flex items-center gap-3 px-4 py-3 hover:bg-[#1A4FA3] cursor-pointer ${
@@ -92,20 +116,44 @@ const Chat = () => {
                 }`}
               >
                 <img
-                  src={chat.avatar}
+                  src='https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-unknown-social-media-user-photo-default-avatar-profile-icon-vector-unknown-social-media-user-184816085.jpg'
                   alt="avatar"
                   className="size-10 rounded-full"
                 />
                 <div>
-                  <p className="font-semibold">{chat.name}</p>
-                  <p className="text-sm text-gray-300">{chat.message}</p>
+                  <p className="font-semibold">{users.username}</p>
+                  <p className="text-sm text-gray-300">{users.email}</p>
                 </div>
               </div>
             ))}
           </div>
         </div>
         {/* chat display */}
-        <div></div>
+        <div className="bg-[#14b8a6] text-white relative">
+          <div className="flex justify-between items-center p-4 border-b-2 border-gray-300 mx-2">
+            <div className="flex items-center gap-3">
+              <img
+                className="size-8 rounded-full"
+                src="https://randomuser.me/api/portraits/men/1.jpg"
+                alt=""
+              />
+              <h1 className="text-lg font-semibold">Richard Stanford</h1>
+              <p className="bg-yellow-400 rounded-full w-3 h-3"></p>
+            </div>
+            <BsExclamationCircle className="size-6 cursor-pointer" />
+          </div>
+          {/* input msg */}
+          <div className="absolute bottom-0 w-full flex items-center gap-2 bg-[#0f766e] p-4">
+            <input
+              type="text"
+              placeholder="Search here.."
+              className="text-white placeholder-white outline-none w-full"
+            />
+            <p className="cursor-pointer">
+              <IoSend className="size-6" />
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
