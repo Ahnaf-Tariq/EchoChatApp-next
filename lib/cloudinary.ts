2e48c0e80b92c5d308d0d0db3d444aac8c084f96
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-08-15 16:44:20 -0700

    cloudinary file configured

---
+++ lib/cloudinary.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+export const uploadCloudinary = async (file: File) => {
+  try {
+    const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
+    const uploadPreset = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET;
+
+    if (!cloudName || !uploadPreset) {
+      console.error("Cloudinary name & preset not found in env file");
+      return null;
+    }
+
+    // form data
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("upload_preset", uploadPreset);
+    formData.append("folder", "chat-app");
+
+    // Upload to cloudinary
+
+    const response = await axios.post(
+      `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+      formData
+    );
+
+    return response.data.secure_url;
+  } catch (error) {
+    console.error("Cloudinary upload error:", error);
+    return null;
+  }
+};
