0df38ead7bc1e3b67c77db03cb5e79d255dbb458
Ahnaf-Hamid <ahnafhamid7@gmail.com>
2025-08-16 23:02:45 -0700

    voice upload cloudinary file made

---
+++ lib/voiceCloudinary.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+
+export const uploadVoiceCloudinary = async (blob: Blob) => {
+  try {
+    const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
+    const uploadPreset = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET;
+
+    if (!cloudName || !uploadPreset) {
+      console.error("Cloudinary name & preset not found in env file");
+      return null;
+    }
+
+    // Convert blob to file
+    const audioFile = new File([blob], `voice-${Date.now()}.webm`, {
+      type: "audio/webm",
+    });
+
+    // form data
+    const formData = new FormData();
+    formData.append("file", audioFile);
+    formData.append("upload_preset", uploadPreset);
+    formData.append("resource_type", "video");
+    formData.append("folder", "chat-app-voices");
+
+    // upload to cloudinary
+    const response = await axios.post(
+      `https://api.cloudinary.com/v1_1/${cloudName}/video/upload`,
+      formData
+    );
+
+    return response.data.secure_url;
+  } catch (error) {
+    console.error("Cloudinary upload voice msg error:" + error);
+    return null;
+  }
+};
